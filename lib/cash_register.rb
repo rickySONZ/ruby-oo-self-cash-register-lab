aa2735171c1d40cc12cb74ededffa05eeb4c5176
Ryan Erricson <ryanerricson@gmail.com>
2021-02-10 10:21:17 -0500

    commit tree 890adfd84880cea77edc62b1375942847b4de3d1

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,49 @@
+require "pry"
 
+class CashRegister
+
+    attr_accessor :total, :discount, :last_transaction
+
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @@mainarray = []
+    end
+
+    
+
+
+    def add_item( title, price, quantity = 1)
+        @title = title
+        @price = price
+        @quantity = quantity
+        previous_total = total
+        @last_transaction = price * quantity
+        newArray = Array.new(quantity){|title| title = @title}
+        @total = last_transaction + total
+        @@mainarray << newArray
+
+    end
+
+
+    def apply_discount
+        if discount > 0
+            discount = 1 - ((@discount.to_f) / 100)
+            @total = total * discount
+            return "After the discount, the total comes to $#{total.to_i}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+
+    def items
+        @@mainarray.flatten
+    end
+    
+    def void_last_transaction
+        @total = total - self.last_transaction
+
+    end
+    
+end
